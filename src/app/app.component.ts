daf56eb209ef52383307aaa557687952f84ed4b6
Pavel Opolovnin <hote_95@bk.ru>
2022-04-07 23:10:35 +0300

    HomeWork-3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Maps';
+  public attributes = {name: 'value1'};
+
+  public onClick() {
+    this.attributes = {name: 'value2'};
+  }
 }
