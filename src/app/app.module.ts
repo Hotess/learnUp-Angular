57c33aa51a7021385c27ec0157483e7c741d2aec
Pavel Opolovnin <hote_95@bk.ru>
2022-04-07 23:26:37 +0300

    Homework-4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
 import {AttributeDirective} from "./attribute.directive";
+import { FormatNumberPipe } from './format-number.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
     AttributeDirective,
+    FormatNumberPipe,
   ],
   imports: [
     BrowserModule,
